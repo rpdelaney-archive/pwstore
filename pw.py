aee59c6ce0f208f85284041d7fc4bd666fbc0b6f
Ryan Delaney <ryan.delaney@gmail.com>
2016-10-31 18:04:15 -0700

    Initial commit

---
+++ pw.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+# Python 3.5.2
+#
+# © Copyright 2016 Ryan Delaney. All rights reserved.
+# This work is distributed WITHOUT ANY WARRANTY whatsoever; without even the
+# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+# See the README file for additional terms and conditions on your use of this
+# software.
+#
+pass
+# vim: ft=python expandtab smarttab shiftwidth=4 softtabstop=4 fileencoding=UTF-8:
