0b5333a0e285c259924ace87dc7473fdf1d6eac1
Ryan Delaney <ryan.delaney@gmail.com>
2016-10-31 18:25:08 -0700

    Add get_gpghome()

--- pw.py
+++ pw.py
@@ -8,6 +8,7 @@
 # See the README file for additional terms and conditions on your use of this
 # software.
 #
+import os
 import gnupg
 
 def get_edata(filepath):
@@ -24,6 +25,20 @@ def decrypt(gpg, edata):
     Take a gpg handler and some data (presumed to be gpg encrypted) and return the decrypted data
     """
     return gpg.decrypt(edata)
+
+
+def get_gpghome():
+    """
+    Try to figure out where the gnupg homedir is
+    """
+
+    if os.environ['GNUPGHOME']:
+        return os.environ['GNUPGHOME']
+
+    homedir = os.environ['HOME'] + '/.gnupg/'
+    tryfile = homedir + 'gpg.conf'
+    if tryfile.is_file():
+        return homedir
 
 
 # vim: ft=python expandtab smarttab shiftwidth=4 softtabstop=4 fileencoding=UTF-8:
