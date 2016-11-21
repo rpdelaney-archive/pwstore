70c965ebd90d351357aed1eea80d1487dffabbbc
Ryan Delaney <ryan.delaney@gmail.com>
2016-11-21 11:20:05 -0800

    get_key test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env python3
 
-pass
+import unittest
+
+import pw
+
+
+class test_get_key(unittest.TestCase):
+
+    def test_get_key(self):
+        jsonstring = '{"4": "5", "6": "7"}'
+
+        result = pw.get_key(jsonstring, "4")
+        self.assertEqual(result, '5')
 
 # vim: ft=python expandtab smarttab shiftwidth=4 softtabstop=4 fileencoding=UTF-8:
