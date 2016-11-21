9a416791bf348d3b3777d5790be15b6154871604
Ryan Delaney <ryan.delaney@gmail.com>
2016-11-21 15:18:50 -0800

    update_key() test

--- tests/test_functions.py
+++ tests/test_functions.py
@@ -13,4 +13,13 @@ class test_get_key(unittest.TestCase):
         result = pw.get_key(jsonstring, "4")
         self.assertEqual(result, '5')
 
+
+class test_update_key(unittest.TestCase):
+
+    def test_update_key(self):
+        inputjson = '{"4": "5", "6": "7"}'
+        targetjson = '{"6": "8", "4": "5"}'
+        result = pw.update_key(inputjson, "6", "8")
+        self.assertEqual(result, targetjson)
+
 # vim: ft=python expandtab smarttab shiftwidth=4 softtabstop=4 fileencoding=UTF-8:
